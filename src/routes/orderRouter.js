25b02108247456700ca5335147cbec3b4d853e1e
westdanika <70970716+westdanika@users.noreply.github.com>
2025-03-21 11:32:10 -0600

    Update (metrics)
    Updated the code to send metrics to grafana. Also added code to authRouter and orderRouter to support gathering those metrics, and added code to the Jest config so that it would ignore the dist directory and stop freaking out about it.

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,48 +1,85 @@
-const express = require('express');
-const config = require('../config.js');
-const { Role, DB } = require('../database/database.js');
-const { authRouter } = require('./authRouter.js');
-const { asyncHandler, StatusCodeError } = require('../endpointHelper.js');
+const express = require("express");
+const config = require("../config.js");
+const { Role, DB } = require("../database/database.js");
+const { authRouter } = require("./authRouter.js");
+const { asyncHandler, StatusCodeError } = require("../endpointHelper.js");
 
 const orderRouter = express.Router();
+const metrics = require("../metrics.js");
 
 orderRouter.endpoints = [
   {
-    method: 'GET',
-    path: '/api/order/menu',
-    description: 'Get the pizza menu',
+    method: "GET",
+    path: "/api/order/menu",
+    description: "Get the pizza menu",
     example: `curl localhost:3000/api/order/menu`,
-    response: [{ id: 1, title: 'Veggie', image: 'pizza1.png', price: 0.0038, description: 'A garden of delight' }],
+    response: [
+      {
+        id: 1,
+        title: "Veggie",
+        image: "pizza1.png",
+        price: 0.0038,
+        description: "A garden of delight"
+      }
+    ]
   },
   {
-    method: 'PUT',
-    path: '/api/order/menu',
+    method: "PUT",
+    path: "/api/order/menu",
     requiresAuth: true,
-    description: 'Add an item to the menu',
+    description: "Add an item to the menu",
     example: `curl -X PUT localhost:3000/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Student", "description": "No topping, no sauce, just carbs", "image":"pizza9.png", "price": 0.0001 }'  -H 'Authorization: Bearer tttttt'`,
-    response: [{ id: 1, title: 'Student', description: 'No topping, no sauce, just carbs', image: 'pizza9.png', price: 0.0001 }],
+    response: [
+      {
+        id: 1,
+        title: "Student",
+        description: "No topping, no sauce, just carbs",
+        image: "pizza9.png",
+        price: 0.0001
+      }
+    ]
   },
   {
-    method: 'GET',
-    path: '/api/order',
+    method: "GET",
+    path: "/api/order",
     requiresAuth: true,
-    description: 'Get the orders for the authenticated user',
+    description: "Get the orders for the authenticated user",
     example: `curl -X GET localhost:3000/api/order  -H 'Authorization: Bearer tttttt'`,
-    response: { dinerId: 4, orders: [{ id: 1, franchiseId: 1, storeId: 1, date: '2024-06-05T05:14:40.000Z', items: [{ id: 1, menuId: 1, description: 'Veggie', price: 0.05 }] }], page: 1 },
+    response: {
+      dinerId: 4,
+      orders: [
+        {
+          id: 1,
+          franchiseId: 1,
+          storeId: 1,
+          date: "2024-06-05T05:14:40.000Z",
+          items: [{ id: 1, menuId: 1, description: "Veggie", price: 0.05 }]
+        }
+      ],
+      page: 1
+    }
   },
   {
-    method: 'POST',
-    path: '/api/order',
+    method: "POST",
+    path: "/api/order",
     requiresAuth: true,
-    description: 'Create a order for the authenticated user',
+    description: "Create a order for the authenticated user",
     example: `curl -X POST localhost:3000/api/order -H 'Content-Type: application/json' -d '{"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]}'  -H 'Authorization: Bearer tttttt'`,
-    response: { order: { franchiseId: 1, storeId: 1, items: [{ menuId: 1, description: 'Veggie', price: 0.05 }], id: 1 }, jwt: '1111111111' },
-  },
+    response: {
+      order: {
+        franchiseId: 1,
+        storeId: 1,
+        items: [{ menuId: 1, description: "Veggie", price: 0.05 }],
+        id: 1
+      },
+      jwt: "1111111111"
+    }
+  }
 ];
 
 // getMenu
 orderRouter.get(
-  '/menu',
+  "/menu",
   asyncHandler(async (req, res) => {
     res.send(await DB.getMenu());
   })
@@ -50,11 +87,11 @@ orderRouter.get(
 
 // addMenuItem
 orderRouter.put(
-  '/menu',
+  "/menu",
   authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
     if (!req.user.isRole(Role.Admin)) {
-      throw new StatusCodeError('unable to add menu item', 403);
+      throw new StatusCodeError("unable to add menu item", 403);
     }
 
     const addMenuItemReq = req.body;
@@ -65,7 +102,7 @@ orderRouter.put(
 
 // getOrders
 orderRouter.get(
-  '/',
+  "/",
   authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
     res.json(await DB.getOrders(req.user, req.query.page));
@@ -74,22 +111,43 @@ orderRouter.get(
 
 // createOrder
 orderRouter.post(
-  '/',
+  "/",
   authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
+    const startTime = new Date();
+
     const orderReq = req.body;
     const order = await DB.addDinerOrder(req.user, orderReq);
     const r = await fetch(`${config.factory.url}/api/order`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', authorization: `Bearer ${config.factory.apiKey}` },
-      body: JSON.stringify({ diner: { id: req.user.id, name: req.user.name, email: req.user.email }, order }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${config.factory.apiKey}`
+      },
+      body: JSON.stringify({
+        diner: { id: req.user.id, name: req.user.name, email: req.user.email },
+        order
+      })
     });
     const j = await r.json();
     if (r.ok) {
       res.send({ order, reportSlowPizzaToFactoryUrl: j.reportUrl, jwt: j.jwt });
+      metrics.addPizzasSold(order.items.length);
+      metrics.addPizzaRevenue(order.items.reduce((acc, item) => acc + item.price, 0));
     } else {
-      res.status(500).send({ message: 'Failed to fulfill order at factory', reportPizzaCreationErrorToPizzaFactoryUrl: j.reportUrl });
+      metrics.addPizzasFailed(order.items.length);
+      res.status(500).send({
+        message: "Failed to fulfill order at factory",
+        reportPizzaCreationErrorToPizzaFactoryUrl: j.reportUrl
+      });
     }
+
+    res.on("finish", () => {
+      const endTime = new Date();
+      const latency = endTime - startTime;
+      console.log(`Request to create pizza took ${latency}ms`);
+      metrics.updatePizzaLatency(latency);
+    });
   })
 );
 
